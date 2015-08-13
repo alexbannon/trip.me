27d391fef572122b0f6a7a964877dd08579c17c9
Karl Sakas <donusdonut@Karls-MacBook-Pro.local>
2015-08-13 19:26:18 -0400

    added map dummy in test

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -3,43 +3,23 @@ $(document).ready(function() {
 
 
   $("h1").on("click", function(){
-    var city = $(".city").val();
-    var state = $(".state").val();
+    var lat     = 37.7808851;
+    var long    = -122.3948632;
 
     // Provide your access token
       L.mapbox.accessToken = 'pk.eyJ1Ijoia2FybGRvbnVzIiwiYSI6Ijg1ZTY5ZDZjMTUxZTdkMzk1Y2MwOTNjNjQwZDMwNTU2In0.WOrmvw7P5KviJbR_u5febw';
     // Create a map in the div #map
-      var geocoder = L.mapbox.geocoder('mapbox.places'),
-          map = L.mapbox.map('map', 'examples.map-h67hf2ic');
-
-      geocoder.query(city + ', ' +  state , showMap);
-
-
-
-      function showMap(err, data) {
-    // The geocoder can return an area, like a city, or a
-    // point, like an address. Here we handle both cases,
-    // by fitting the map bounds to an area or zooming to a point.
-    if (data.latlng) {
-        map.setView([data.latlng[0], data.latlng[1]], 13);
-        L.marker([data.latlng[0], data.latlng[1]], {
+      var map = L.mapbox.map('map', 'mapbox.streets').setView([lat, long], 13);
+        L.marker([lat, long], {
           icon: L.mapbox.marker.icon({
             'marker-size': 'large',
-            'marker-symbol': 'danger',
+            'marker-symbol': 'camera',
             'marker-color': '#fa0'
           })
         }).addTo(map);
-
-    }}
-
-    });
-
-
-
-
-
-
-
+      var cir = L.circle([lat, long], 1000).addTo(map);
 
 
 });
+
+    });
