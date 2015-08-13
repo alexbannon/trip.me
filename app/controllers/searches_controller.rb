7b924aff8efd71059d8a6315a4a81996d67f8090
Karl Sakas <donusdonut@Karls-MacBook-Pro.local>
2015-08-13 16:37:34 -0400

    used instagram in place of token

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,24 +1,8 @@
 class SearchesController < ApplicationController
 
   def homepage
-
+    @instagram = Instagram.tag_recent_media('victory', {:count => 10})
   end
 
-  def search_results
-    response = HTTParty.get("https://api.instagram.com/v1/tags/potato/media/recent?access_token=")
-    @photos = []
-    @photos.push(response["data"][0]["images"]["standard_resolution"]["url"])
-    @photos.push(response["data"][1]["images"]["standard_resolution"]["url"])
-    @photos << response["data"][2]["images"]["standard_resolution"]["url"]
-    @photos << response["data"][3]["images"]["standard_resolution"]["url"]
-    @photos << response["data"][4]["images"]["standard_resolution"]["url"]
-    @photos << response["data"][5]["images"]["standard_resolution"]["url"]
-    @photos << response["data"][6]["images"]["standard_resolution"]["url"]
-    @photos << response["data"][7]["images"]["standard_resolution"]["url"]
-    @photos << response["data"][8]["images"]["standard_resolution"]["url"]
-    @photos << response["data"][9]["images"]["standard_resolution"]["url"]
-    puts "-" * 50
-    puts @photos
-  end
 
 end
