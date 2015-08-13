933fbb5d008f5c941bd67650b48d5dd5bbc8716f
Alex Bannon <alexbannon@gmail.com>
2015-08-13 16:53:19 -0400

    added user search and displaying photos from instagram api on search_results page

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,8 +1,16 @@
 class SearchesController < ApplicationController
 
   def homepage
-    @instagram = Instagram.tag_recent_media('victory', {:count => 10})
+    @instagram_photos = Instagram.tag_recent_media('victory', {:count => 10})
   end
 
+  def search_results
+    @usersearch = params[:guestsearch]
+    @instagram_photos = Instagram.tag_recent_media(@usersearch, {:count => 10})
 
+    puts "-" * 100
+    puts @usersearch
+    puts "-" * 100
+    puts @instagram_photos
+  end
 end
