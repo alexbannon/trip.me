5b9621413bb1ad18e3b74401ea48798e3612abe7
Karl Sakas <donusdonut@Karls-MacBook-Pro.local>
2015-08-15 15:22:09 -0400

    changed homepage results to low resolution. solved img broken link prob.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -7,7 +7,7 @@ class SearchesController < ApplicationController
   def search_results
 
     @usersearch = params[:guestsearch]
-    @instagram_photos = Instagram.tag_recent_media(@usersearch, {:count => 50})
+    @instagram_photos = Instagram.tag_recent_media(@usersearch, {:count => 33})
     @photos_with_location = []
     for photo in @instagram_photos do
       if photo.location != nil
@@ -18,6 +18,15 @@ class SearchesController < ApplicationController
         end
       end
     end
+    puts "-" * 100
+    puts @usersearch
+    puts "-" * 100
+    puts @instagram_photos.length
+    puts "-" * 100
+    puts @photos_with_location
+    puts "-" * 100
+    puts @photos_with_location[0].location[15...80]
+
   end
 
   def geo_results
@@ -25,6 +34,13 @@ class SearchesController < ApplicationController
     @location_array = @location.split("_")
     @lat = @location_array[1].strip
     @long = @location_array[3].strip
+    puts "-" * 100
+    puts @location
+    puts "-" * 100
+    puts @lat
+    puts "-" * 100
+    puts @long
+    puts "-" * 100
     @instagram_photos = Instagram.media_search(@lat, @long, {:count => 1000})
     @photos_with_geo = []
     for photo in @instagram_photos do
@@ -36,5 +52,12 @@ class SearchesController < ApplicationController
         end
       end
     end
+    puts "-" * 100
+    puts @usersearch
+    puts "-" * 100
+    puts @instagram_photos.length
+    puts "-" * 100
+    puts @photos_with_geo
+
   end
 end
